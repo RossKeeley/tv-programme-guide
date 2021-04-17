6d0d47bad5ccbb40028b3f18c312627915157b5e
Ross Keeley <rjkeeley@gmail.com>
2021-04-17 12:47:36 +0100

    Added map method for channels and channel movies

--- src/components/Channel/index.js
+++ src/components/Channel/index.js
@@ -177,7 +177,21 @@ const Channel = () => {
     
     return (
         <div>
-
+            (data || []).map((channel) => {
+                return (
+                    <div>
+                        <h3>{channel.name}</h3>
+                        (channel.movies || []).map((movie) => {
+                            return (
+                                <div>
+                                    <p>{movie.title}</p>
+                                    
+                                </div>
+                            )
+                        })
+                    </div>
+                )
+            })
         </div>
     )
 }
